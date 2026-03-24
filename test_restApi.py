b682b4c82f9fa4347c26e7e6652be704fd5dc746
siddeshob <siddeshaob2001@gmail.com>
2026-03-24 19:04:54 +0530

    api test added

---
+++ test_restApi.py
@@ -0,0 +1,21 @@
+import requests
+import pytest
+
+BASE_URL = "https://api.restful-api.dev"
+
+@pytest.mark.smoke
+class TestAPITest:
+
+    @pytest.mark.getdata
+    def test_get_mobile_list(self):
+        response = requests.get(f"{BASE_URL}/objects")
+
+        # Status code validation
+        assert response.status_code == 200
+
+        # Response validation
+        json_res = response.json()
+        assert isinstance(json_res, list)
+
+        # Optional: check data exists
+        assert len(json_res) > 0
